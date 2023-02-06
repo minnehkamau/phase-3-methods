18df1b18163bc22507b4948071ffdab5522ef63c
minnehkamau <minnieches08@gmail.com>
2023-02-06 23:04:48 +0300

    Done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+def greet(name)
+    puts "Hello, #{name}!"
+    
+end
+def greet_with_default(name = nil)
+    if name.nil? ||name.empty?
+    name = "programmer"
+    end
+    puts "Hello, #{name}!"
+end
+def add(num1, num2)
+    num1 + num2
+end
+def halve(num)
+   if num.is_a?(Integer)
+    num/2
+   else
+    nil
+   end
+end
+
